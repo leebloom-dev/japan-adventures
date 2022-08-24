f5b15f1aef3cd9f27b9c8aa0b6dfeb121707f65d
Lee <leebloomdev@gmail.com>
2022-08-24 10:33:41 -0400

    Starte Code using Spring Boot

---
+++ src/main/java/com/japanadventures/japanadventures/JapanAdventuresApplication.java
@@ -0,0 +1,13 @@
+package com.japanadventures.japanadventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanAdventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanAdventuresApplication.class, args);
+	}
+
+}
